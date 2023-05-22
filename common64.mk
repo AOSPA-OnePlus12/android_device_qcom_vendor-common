2ba6dde573ffa3c81cb6ec7a6a5a26fdeb1e2461
Kartik Kumar Mathur <quic_kartmath@quicinc.com>
2023-05-22 10:13:39 -0700

    Priv-app permissions on Vendor Side
    Adding priv-app permissions on vendor side to resolve bootup crash
    caused by camera-aon.
    
    Change-Id: I47ce6287448f53ef9b7db1dddcbe2dd42a96daa3

--- common64.mk
+++ common64.mk
@@ -26,7 +26,7 @@ PRODUCT_COPY_FILES += \
     device/qcom/common/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 PRODUCT_COPY_FILES += \
-    device/qcom/common/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+    device/qcom/vendor-common/privapp-permissions-qti-vendor.xml:vendor/etc/permissions/privapp-permissions-qti-vendor.xml
 
 PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
 
